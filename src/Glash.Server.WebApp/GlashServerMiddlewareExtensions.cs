1be5cefca00ba5aa43d27a26e736a82873362cea
张鹏 <scbeta@qq.com>
2023-03-06 16:45:55 +0800

    Prepare to release version 1.0.0

---
+++ src/Glash.Server.WebApp/GlashServerMiddlewareExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Glash.Core.Server;
+using Quick.Protocol;
+
+namespace Microsoft.AspNetCore.Builder
+{
+    public static class GlashServerMiddlewareExtensions
+    {
+        private static GlashServer glashServer;
+        private static Quick.Protocol.WebSocket.Server.AspNetCore.QpWebSocketServer qpServer;
+
+        public static IApplicationBuilder UseGlashServer(this IApplicationBuilder app, string path, string password)
+        {
+            var serverOptions = new Quick.Protocol.WebSocket.Server.AspNetCore.QpWebSocketServerOptions()
+            {
+                Path = path,
+                Password = password,
+                ServerProgram = "Glash.Server"
+            };
+            glashServer = new GlashServer();
+            glashServer.HandleServerOptions(serverOptions);
+
+            app.UseQuickProtocol(serverOptions, out qpServer);
+            qpServer.Start();
+            return app;
+        }
+    }
+}
