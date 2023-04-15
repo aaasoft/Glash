f61aaa4c27d2b6dd84662eabaab4f9ba2336c736
张鹏 <scbeta@qq.com>
2023-04-15 22:30:23 +0800

    Add start remote desktop feature.

--- src/Glash.Blazor.Client/ProxyTypes/RDP.cs
+++ src/Glash.Blazor.Client/ProxyTypes/RDP.cs
@@ -1,4 +1,9 @@
 ﻿using Glash.Client;
+using System.Diagnostics;
+using System.Net;
+using System.Runtime.Versioning;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace Glash.Blazor.Client.ProxyTypes
 {
@@ -18,6 +23,7 @@ namespace Glash.Blazor.Client.ProxyTypes
 
         public override string Icon => "fa fa-desktop";
 
+        [SupportedOSPlatform("windows")]
         public override ProxyTypeButton[] GetButtons()
         {
             return new ProxyTypeButton[]
@@ -30,9 +36,45 @@ namespace Glash.Blazor.Client.ProxyTypes
             };
         }
 
+        /// <summary>
+        /// 获取RDP密码
+        /// </summary>
+        [SupportedOSPlatform("windows")]
+        private static string GetRdpPassWord(string pw)
+        {
+            byte[] secret = Encoding.Unicode.GetBytes(pw);
+            byte[] encryptedSecret = ProtectedData.Protect(secret, null, DataProtectionScope.LocalMachine);
+            return BitConverter.ToString(encryptedSecret).Replace("-", string.Empty);
+        }
+
+        [SupportedOSPlatform("windows")]
         private void StartRDP(ProxyRuleContext proxyRuleContext)
         {
-
+            var sb = new StringBuilder();
+            var localIpAddress = proxyRuleContext.Config.LocalIPAddress;
+            if (localIpAddress == IPAddress.Any.ToString())
+                localIpAddress = IPAddress.Loopback.ToString();
+            if (localIpAddress == IPAddress.IPv6Any.ToString())
+                localIpAddress = IPAddress.IPv6Loopback.ToString();
+            sb.AppendLine($"full address:s:{localIpAddress}:{proxyRuleContext.LocalPort}");
+            sb.AppendLine($"username:s:{User}");
+            sb.AppendLine($"password 51:b:{GetRdpPassWord(Password)}");
+            var tmpFile = Path.GetTempFileName();
+            var tmpFileInfo = new FileInfo(tmpFile);
+            try
+            {
+                File.WriteAllText(tmpFile, sb.ToString());
+                Process.Start("mstsc.exe", tmpFile);
+                Task.Delay(1000).ContinueWith(t =>
+                {
+                    File.Delete(tmpFile);
+                });                
+            }
+            catch
+            {
+                File.Delete(tmpFile);
+                throw;
+            }
         }
     }
 }
