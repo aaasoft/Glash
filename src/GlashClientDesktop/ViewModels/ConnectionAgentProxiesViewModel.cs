468a66288f9c3cd93ad6e30321981ef472463674
张鹏 <scbeta@qq.com>
2026-06-24 09:52:55 +0800

    UI优化中。。。

--- src/GlashClientDesktop/ViewModels/ConnectionAgentProxiesViewModel.cs
+++ src/GlashClientDesktop/ViewModels/ConnectionAgentProxiesViewModel.cs
@@ -39,25 +39,69 @@ public class ConnectionAgentProxiesViewModel : ViewModelBase
     public ProxyRuleContext CurrentRule
     {
         get => _CurrentRule;
-        set => this.RaiseAndSetIfChanged(ref _CurrentRule, value);
+        set
+        {
+            this.RaiseAndSetIfChanged(ref _CurrentRule, value);
+            if (value == null)
+                CurrentRuleEnable = false;
+            CurrentRuleEnable = value.Config.Enable;
+        }
     }
+
+    private bool _CurrentRuleEnable = false;
+    public bool CurrentRuleEnable
+    {
+        get => _CurrentRuleEnable;
+        set => this.RaiseAndSetIfChanged(ref _CurrentRuleEnable, value);
+    }
+
+    public ReactiveCommand<Unit, Unit> AddCommand { get; }
+    public ReactiveCommand<Unit, Unit> EditCommand { get; }
+    public ReactiveCommand<Unit, Unit> DeleteCommand { get; }
 
     public ReactiveCommand<Unit, Unit> StartCommand { get; }
     public ReactiveCommand<Unit, Unit> StopCommand { get; }
 
     public ConnectionAgentProxiesViewModel()
     {
+        AddCommand = ReactiveCommand.CreateFromTask(ExecuteCommand_Add);
+        EditCommand = ReactiveCommand.CreateFromTask(ExecuteCommand_Edit, this.WhenAnyValue(
+                x => x.CurrentRule,
+                x => x.CurrentRuleEnable,
+                (currentRule, currentRuleEnable) => currentRule!=null && !currentRuleEnable));
+        DeleteCommand = ReactiveCommand.CreateFromTask(ExecuteCommand_Delete, this.WhenAnyValue(
+                x => x.CurrentRule,
+                x => x.CurrentRuleEnable,
+                (currentRule, currentRuleEnable) => currentRule!=null && !currentRuleEnable));
+
         StartCommand = ReactiveCommand.CreateFromTask(ExecuteCommand_Start);
         StopCommand = ReactiveCommand.CreateFromTask(ExecuteCommand_Stop);
+    }
+
+    public async Task ExecuteCommand_Add()
+    {
+
+    }
+
+    public async Task ExecuteCommand_Edit()
+    {
+
+    }
+
+    public async Task ExecuteCommand_Delete()
+    {
+
     }
 
     public async Task ExecuteCommand_Start()
     {
         await ConnectionContext.GlashClient.EnableProxyRule(CurrentRule.Config.Id);
+        CurrentRuleEnable = true;
     }
 
     public async Task ExecuteCommand_Stop()
     {
         await ConnectionContext.GlashClient.DisableProxyRule(CurrentRule.Config.Id);
+        CurrentRuleEnable = false;
     }
 }
