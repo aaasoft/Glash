31a81bb89aa5aaa41243fa994b69fa5f776a1c28
张鹏 <scbeta@qq.com>
2026-09-22 10:19:09 +0800

    增加G.D这个类和命名空间的设计说明

--- Glash/Core/TunnelDataAvailable.cs
+++ Glash/Core/TunnelDataAvailable.cs
@@ -2,12 +2,16 @@
 using System.Text.Json.Serialization;
 using System.Text.Json.Serialization.Metadata;
 
+//命令空间G是Glash的缩写，这里使用缩写是为了最大程序减小代理数据类名(G.D)的长度以节省网络流量，提升传输效率
 namespace G
 {
     [JsonSerializable(typeof(D))]
     [JsonSourceGenerationOptions(DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull)]
     internal partial class GDSerializerContext : JsonSerializerContext { }
 
+    /// <summary>
+    /// 类名D是Data的缩写，这里使用缩写是为了最大程序减小代理数据类名(G.D)的长度以节省网络流量，提升传输效率
+    /// </summary>
     public class D : AbstractQpSerializer<D>
     {
         protected override JsonTypeInfo<D> GetTypeInfo() => GDSerializerContext.Default.D;
